a76b0ed697240c2c4ccd49c7d80e33900e94c60f
tibberous <TrentTompkins@gmail.com>
2013-10-09 16:02:57 -0400

    Base

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title>Smashing HTML5!</title>
+		<script src="js/jquery-2.0.3.min.js"></script>
+		<link rel="stylesheet" href="bootstrap.min.css">
+		<script src="js/bootstrap.js"></script>
+		<!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+	</head>
+ 	<body>
+ 		<h1>Hello</h1>
+ 		
+ 		
+	</body>
+</html>
